28292cf1d4ab73882b0e0b5fd0c605b61b491536
Dayer <dayerxr.cher@gmail.com>
2026-05-22 22:27:10 +0800

    chore: updated dependencies and shared theme

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -2,6 +2,14 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
+  palette: {
+    background: {
+      default:
+        "linear-gradient(90deg,rgba(187, 247, 250, 1) 0%, rgba(130, 222, 255, 1) 51%, rgba(187, 247, 250, 1) 100%)",
+      paper:
+        "linear-gradient(90deg,rgba(187, 247, 250, 1) 0%, rgba(130, 222, 255, 1) 51%, rgba(187, 247, 250, 1) 100%)"
+    }
+  },
   typography: {
     fontFamily: "var(--font-roboto)"
   }
