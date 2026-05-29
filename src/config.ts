63c0804cfec17254cb1266dee431202b0510fe75
Dayer <dayerxr.cher@gmail.com>
2026-05-29 14:53:30 +0800

    chore: minor config and types changes

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const GITHUB_CLIENT_ID: string = process.env.GITHUB_CLIENT_ID as string;
 export const GITHUB_CLIENT_SECRET: string = process.env
   .GITHUB_CLIENT_SECRET as string;
 
-export const COINGECKO_SERVICE_URL: string = process.env
-  .COINGECKO_SERVICE_URL as string;
 export const COINGECKO_API_KEY: string = process.env
   .COINGECKO_API_KEY as string;
+
+export const ALCHEMY_API_KEY: string = process.env.ALCHEMY_API_KEY as string;
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,35 @@
+import { APIPromise } from "@coingecko/coingecko-typescript";
+import MarketGetResponse from "@coingecko/coingecko-typescript";
 import { LucideIcon } from "lucide-react";
 
 export type SidebarStateTypes = {
   sidebarState: boolean;
 };
+
+/*export type CollectionTypes = {
+  id: string;
+  name: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  total_volume: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_percentage_24h: number;
+  total_supply: number;
+  last_updated: string;
+};
+*/
+
+export type CollectionTypes = {
+  data: APIPromise<MarketGetResponse>;
+};
+
+export interface TrendingCollectionsProps {
+  selectedCollection: string;
+  onSelectCollection: (id: string) => void;
+}
 
 export type CalculateHeaderTypes = {
   title: string;
