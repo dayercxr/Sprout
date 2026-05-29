d7b85e8f8fdd926a3cd3b76b4784a16a27543514
Dayer <dayerxr.cher@gmail.com>
2026-05-29 15:53:07 +0800

    chore: removed irrelevant types

--- src/types.ts
+++ src/types.ts
@@ -6,22 +6,6 @@ export type SidebarStateTypes = {
   sidebarState: boolean;
 };
 
-/*export type CollectionTypes = {
-  id: string;
-  name: string;
-  image: string;
-  current_price: number;
-  market_cap: number;
-  market_cap_rank: number;
-  total_volume: number;
-  high_24h: number;
-  low_24h: number;
-  price_change_percentage_24h: number;
-  total_supply: number;
-  last_updated: string;
-};
-*/
-
 export type CollectionTypes = {
   data: APIPromise<MarketGetResponse>;
 };
