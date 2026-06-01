89630e825bb70de0871e0a996b9948ab8592a769
Dayer <dayerxr.cher@gmail.com>
2026-06-01 14:50:25 +0800

    chore: minor config env changes

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,3 @@
-export const PG_HOST: string = process.env.PG_HOST as string;
-export const PG_DATABASE: string = process.env.PG_DATABASE as string;
-export const PG_PASSWORD: string = process.env.PG_PASSWORD as string;
-export const PG_USER: string = process.env.PG_USER as string;
-export const PG_PORT: string = process.env.PG_PORT as string;
-
 export const DATABASE_URL: string = process.env.DATABASE_URL as string;
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
@@ -14,9 +8,8 @@ export const GITHUB_CLIENT_ID: string = process.env.GITHUB_CLIENT_ID as string;
 export const GITHUB_CLIENT_SECRET: string = process.env
   .GITHUB_CLIENT_SECRET as string;
 
-export const NEXTAUTH_SECRET: string = process.env.NEXTAUTH_SECRET as string;
+export const BETTER_AUTH_SECRET: string = process.env
+  .BETTER_AUTH_SECRET as string;
 
 export const COINGECKO_API_KEY: string = process.env
   .COINGECKO_API_KEY as string;
-
-export const ALCHEMY_API_KEY: string = process.env.ALCHEMY_API_KEY as string;
