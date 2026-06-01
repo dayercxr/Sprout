7a38657e98dd389aee28f6c4fb11006e731ba635
Dayer <dayerxr.cher@gmail.com>
2026-06-01 14:49:19 +0800

    chore: path changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { DATABASE_URL } from "@/config";
 
 export default defineConfig({
   out: "./migrations",
-  schema: "./src/db/auth-schema.ts",
+  schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: DATABASE_URL
