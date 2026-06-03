119e02db30a61b53c80c1c0735d3a1c873d98170
Dayer <dayerxr.cher@gmail.com>
2026-06-03 14:05:54 +0800

    fix: template structure for profile page

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,1 +1,8 @@
 import { AuthServerHandler } from "@/libs/auth/auth-server";
+import { Box } from "@mui/material";
+
+export default async function Profile() {
+  await AuthServerHandler.requireAuth();
+
+  return <Box></Box>;
+}
