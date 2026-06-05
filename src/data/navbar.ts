fc5b82aaa2a72c5662d8b7d2f29e55693a4470fa
Dayer <dayerxr.cher@gmail.com>
2026-06-05 23:29:59 +0800

    feat: added data for navbar

--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -1,7 +1,19 @@
-import { LayoutDashboard, Calculator, Star } from "lucide-react";
+import {
+  LayoutDashboard,
+  Calculator,
+  Star,
+  Home,
+  CircleUser
+} from "lucide-react";
 
-export const NavbarData = [
-  { path: "/dashboard", label: "Dashboard", icon: LayoutDashboard },
-  { path: "/calculator", label: "Calculator", icon: Calculator },
-  { path: "/watchlist", label: "Watchlist", icon: Star }
-];
+export const NavbarData = {
+  menu: [
+    { path: "/profile", label: "Profile", Icon: CircleUser },
+    { path: "/", label: "Home", Icon: Home }
+  ],
+  navigation: [
+    { path: "/dashboard", label: "Dashboard", icon: LayoutDashboard },
+    { path: "/calculator", label: "Calculator", icon: Calculator },
+    { path: "/watchlist", label: "Watchlist", icon: Star }
+  ]
+};
