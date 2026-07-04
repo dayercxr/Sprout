2bfd65582ff1e438aee743ae783f2cee28db5340
dayercxr <dayerxr.cher@gmail.com>
2026-07-04 16:12:17 +0800

    chore: minor additions to name attribute of textfields

--- src/data/calculator.ts
+++ src/data/calculator.ts
@@ -11,9 +11,17 @@ export const CalculatorData = {
       logo: TrendingUp,
       logoColor: "#a855f7",
       inputs: [
-        { label: "Purchase Price (ETH)", placeholder: "0.00" },
-        { label: "Current Price (ETH)", placeholder: "0.00" },
-        { label: "Quantity", placeholder: "0" }
+        {
+          label: "Purchase Price (ETH)",
+          placeholder: "0.00",
+          name: "purchasePrice"
+        },
+        {
+          label: "Current Price (ETH)",
+          placeholder: "0.00",
+          name: "currentPrice"
+        },
+        { label: "Quantity", placeholder: "0", name: "quantity" }
       ],
       displays: ["ROI", "Profit/Loss", "Total Value"]
     },
@@ -23,10 +31,10 @@ export const CalculatorData = {
       logo: DollarSign,
       logoColor: "#4ade80",
       inputs: [
-        { label: "Buy Price (ETH)", placeholder: "0.00" },
-        { label: "Sell Price (ETH)", placeholder: "0.00" },
-        { label: "Royalty (%)", placeholder: "0" },
-        { label: "Gas Fee (ETH)", placeholder: "0.00" }
+        { label: "Buy Price (ETH)", placeholder: "0.00", name: "buyPrice" },
+        { label: "Sell Price (ETH)", placeholder: "0.00", name: "sellPrice" },
+        { label: "Royalty (%)", placeholder: "0", name: "royalty" },
+        { label: "Gas Fee (ETH)", placeholder: "0.00", name: "gas" }
       ],
       displays: ["Net Profit", "Royalty Fee", "Total Costs"]
     },
@@ -36,9 +44,13 @@ export const CalculatorData = {
       logo: Calculator,
       logoColor: "#60a5fa",
       inputs: [
-        { label: "Initial Investment (ETH)", placeholder: "0.00" },
-        { label: "Target Return", placeholder: "0.00" },
-        { label: "Timeframe (months)", placeholder: "0" }
+        {
+          label: "Initial Investment (ETH)",
+          placeholder: "0.00",
+          name: "initialInvestment"
+        },
+        { label: "Target Return", placeholder: "0.00", name: "targetReturn" },
+        { label: "Timeframe (months)", placeholder: "0", name: "time" }
       ],
       displays: ["Target Value", "Required Growth", "Monthly Return Needed"]
     }
